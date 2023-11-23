6ad200e11838bf60b7463e4fa484a5f525b3938e
Sapar S <sapar26@mail.ru>
2023-11-23 15:49:16 +0300

    создал валидацию с библиотекой Pristine

--- js/active.js
+++ js/active.js
@@ -1,11 +1,10 @@
-import {renderOfferCards, clearOfferCards} from './popup.js';
+import {renderOfferCards} from './popup.js';
 
 // находим форму заполнения
 const getForm = document.querySelector('.ad-form');
 // находим содержимое формы
 const fieldSets = getForm.querySelectorAll('.ad-form__element');
 const dropAvatar = getForm.querySelector('#avatar');
-
 // находим фильтр заполнения
 const getMapFiltres = document.querySelector('.map__filters');
 // находим содержимое филтра
@@ -15,9 +14,10 @@ const selectFeature = document.querySelector('#housing-features').querySelectorA
 
 const noActive = (element) => {
   for (let i = 0; i < element.length; i++) {
-    element[i].setAttribute('disabled', '');
+    element[i].setAttribute('disabled', true);
   }
 };
+
 
 //создали неактивное состояние событие
 const onContentLoad = (evt) => {
@@ -27,15 +27,16 @@ const onContentLoad = (evt) => {
   noActive(selectFilter);
   noActive(selectFeature);
   noActive(fieldSets);
-  dropAvatar.setAttribute('disabled', '');
+  dropAvatar.setAttribute('disabled', true);
 };
 
 // создали функцию для загрузки неактивного состояние страницы - недоступны форма и фильтры
-function closeForm () {
-  clearOfferCards();
-  document.addEventListener('DOMContentLoaded', onContentLoad);
-}
-closeForm();
+// function closeForm () {
+//   clearOfferCards();
+//   document.addEventListener('DOMContentLoaded', onContentLoad);
+// }
+//closeForm();
+
 
 // создали функцию для загрузки активного состояния страницы - доступны форма и фильтры
 // eslint-disable-next-line no-unused-vars
@@ -43,5 +44,6 @@ function openForm () {
   renderOfferCards();
   document.removeEventListener('DOMContentLoaded', onContentLoad);
 }
-//openForm();
+openForm();
+
 
